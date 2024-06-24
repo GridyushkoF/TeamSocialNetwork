dddd492080c2346404e416779f731a1611ee6097
fiodor_gridiushko <grid467@gmail.com>
2024-06-24 12:43:50 +0300

    Updated logic: now in PostService and CommentService id from dto ignoring, using from path variable. Author id ignoring, using auth user (while creating posts and comments)

--- post-service/src/main/java/ru/skillbox/postservice/controller/LikeController.java
+++ post-service/src/main/java/ru/skillbox/postservice/controller/LikeController.java
@@ -3,7 +3,6 @@ package ru.skillbox.postservice.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.skillbox.postservice.service.LikeService;
 
@@ -12,42 +11,32 @@ import ru.skillbox.postservice.service.LikeService;
 @RequestMapping("${app.apiPrefix}/post")
 public class LikeController {
     private final LikeService likeService;
+
     @PostMapping("/{id}/like")
     @ResponseStatus(HttpStatus.CREATED)
-    public void likePost(
-            @PathVariable("id") Long postId,
-            HttpServletRequest request
-    ) {
+    public void likePost(@PathVariable("id") Long postId, HttpServletRequest request) {
         Long currentAuthUserId = Long.parseLong(request.getHeader("id"));
-        likeService.likePost(postId,currentAuthUserId);
+        likeService.likePost(postId, currentAuthUserId);
     }
+
     @DeleteMapping("/{id}/like")
     @ResponseStatus(HttpStatus.OK)
-    public void unLikePost(
-            @PathVariable("id") Long postId,
-            HttpServletRequest request
-    ) {
+    public void unLikePost(@PathVariable("id") Long postId, HttpServletRequest request) {
         Long currentAuthUserId = Long.parseLong(request.getHeader("id"));
-        likeService.unlikePost(postId,currentAuthUserId);
+        likeService.unlikePost(postId, currentAuthUserId);
     }
+
     @PostMapping("/{id}/comment/{commentId}/like")
     @ResponseStatus(HttpStatus.CREATED)
-    public void likeComment(
-            @PathVariable("id") Long postId,
-            @PathVariable("commentId") Long commentId,
-            HttpServletRequest request
-    ) {
+    public void likeComment(@PathVariable("id") Long postId, @PathVariable("commentId") Long commentId, HttpServletRequest request) {
         Long currentAuthUserId = Long.parseLong(request.getHeader("id"));
-        likeService.likeComment(postId,commentId,currentAuthUserId);
+        likeService.likeComment(postId, commentId, currentAuthUserId);
     }
+
     @DeleteMapping("/{id}/comment/{commentId}/like")
     @ResponseStatus(HttpStatus.OK)
-    public void unLikeComment(
-            @PathVariable("id") Long postId,
-            @PathVariable("commentId") Long commentId,
-            HttpServletRequest request
-    ) {
+    public void unLikeComment(@PathVariable("id") Long postId, @PathVariable("commentId") Long commentId, HttpServletRequest request) {
         Long currentAuthUserId = Long.parseLong(request.getHeader("id"));
-        likeService.unlikeComment(postId,commentId,currentAuthUserId);
+        likeService.unlikeComment(postId, commentId, currentAuthUserId);
     }
 }
