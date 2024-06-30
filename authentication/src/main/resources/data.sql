314dc8e88a01c2e686ddd4a2361f2e02aa8cc6a8
Sergei Ragaev <sergei.ragaev@gmail.com>
2024-06-30 16:01:23 +0300

    - changed password of preinstalled admin to admin123

--- authentication/src/main/resources/data.sql
+++ authentication/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into "users" (id, reg_date, email, first_name, last_name, password, role)
-    values (1, '2024-06-30 12:06:47.297523', 'admin@socialnetwork.com', 'Firstname', 'Lastname', '$2a$10$56fEHY86Cg3KZdBG4LIFV.bfv2Mhox0ptayasvG5zQgo15fQde.9G', 'ADMIN');
+    values (1, '2024-06-30 12:06:47.297523', 'admin@socialnetwork.com', 'Firstname', 'Lastname', '$2a$10$jKwjnyGxN/oBixbpgyG7rug2QB/k6U7/35JyFPChXv6W5/QUvCa/u', 'ADMIN');
 ALTER SEQUENCE users_id_seq RESTART WITH 2;
