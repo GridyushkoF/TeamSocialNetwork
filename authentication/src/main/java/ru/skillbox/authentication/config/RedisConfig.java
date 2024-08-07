d22f90e20d592d175078d60f3a8a8ca3cf1c2bc0
fiodor_gridiushko <grid467@gmail.com>
2024-08-07 17:17:59 +0300

    auth redis fix

--- authentication/src/main/java/ru/skillbox/authentication/config/RedisConfig.java
+++ authentication/src/main/java/ru/skillbox/authentication/config/RedisConfig.java
@@ -1,13 +1,13 @@
-package ru.skillbox.authentication.service.security;
+package ru.skillbox.authentication.config;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import ru.skillbox.authentication.model.entity.RefreshToken;
@@ -16,26 +16,23 @@ import java.time.Duration;
 import java.util.Collections;
 
 @Configuration
-@EnableRedisRepositories(keyspaceConfiguration = RedisConfiguration.RefreshTokenKeyspaceConfiguration.class,
-enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
-public class RedisConfiguration {
+@EnableRedisRepositories(keyspaceConfiguration = RedisConfig.RefreshTokenKeyspaceConfiguration.class,
+        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+public class RedisConfig {
 
-    @Value("security.jwt.refreshTokenExpiration")
+    @Value("${security.jwt.refreshTokenExpiration}")
     private Duration refreshTokenExpiration;
 
     @Bean
-    public RedisProperties redisProperties() {
-        return new RedisProperties();
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
     }
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory(RedisProperties redisProperties) {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-
-        configuration.setHostName(redisProperties.getHost());
-        configuration.setPort(redisProperties.getPort());
-
-        return new JedisConnectionFactory(configuration);
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
     }
 
     public class RefreshTokenKeyspaceConfiguration extends KeyspaceConfiguration {
@@ -44,7 +41,6 @@ public class RedisConfiguration {
         @Override
         protected Iterable<KeyspaceSettings> initialConfiguration() {
             KeyspaceSettings keyspaceSettings = new KeyspaceSettings(RefreshToken.class, REFRESH_TOKEN_KEYSPACE);
-
             keyspaceSettings.setTimeToLive(refreshTokenExpiration.getSeconds());
             return Collections.singleton(keyspaceSettings);
         }
