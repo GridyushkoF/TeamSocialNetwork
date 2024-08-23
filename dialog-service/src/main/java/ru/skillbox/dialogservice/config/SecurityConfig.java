a5f220fc4f044c81cfa39975c659fed2ce3e6579
Sergei Ragaev <sergei.ragaev@gmail.com>
2024-08-23 20:46:06 +0300

    prometheus endpoint in dialog-service opened

--- dialog-service/src/main/java/ru/skillbox/dialogservice/config/SecurityConfig.java
+++ dialog-service/src/main/java/ru/skillbox/dialogservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.skillbox.dialogservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/swagger-ui/**", "v3/api-docs",
                                         "v3/api-docs/*", "/swagger-resources/*").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .build();
