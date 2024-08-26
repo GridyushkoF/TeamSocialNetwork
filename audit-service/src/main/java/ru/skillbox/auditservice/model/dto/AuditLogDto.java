46352e5bae8d3da662b2850064ee0729b134e3c0
Sverreskeggi <alexanderpoilov85@gmail.com>
2024-08-26 23:37:02 +0300

    Audit controller to filter results for administrators

--- audit-service/src/main/java/ru/skillbox/auditservice/model/dto/AuditLogDto.java
+++ audit-service/src/main/java/ru/skillbox/auditservice/model/dto/AuditLogDto.java
@@ -1,31 +1,24 @@
-package ru.skillbox.auditservice.model.entity;
+package ru.skillbox.auditservice.model.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.experimental.Accessors;
 import ru.skillbox.commonlib.event.audit.ActionType;
 
 import java.time.ZonedDateTime;
 
-@Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "audit_logs")
-@Data
-public class AuditLog {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Accessors(chain = true)
+public class AuditLogDto {
     private Long id;
     private String entityName;
     private Long entityId;
-    @Enumerated(EnumType.STRING)
     private ActionType actionType;
     private Long userId;
-    @CreationTimestamp
     private ZonedDateTime createdAt;
-
 }
