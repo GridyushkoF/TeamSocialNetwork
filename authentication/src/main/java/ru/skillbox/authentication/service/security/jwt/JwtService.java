c9256658d23034f4ba13a9fb342cabb3f7f66fac
Sergei Ragaev <sergei.ragaev@gmail.com>
2024-09-01 17:43:37 +0300

    revert to using issuer in jwt

--- authentication/src/main/java/ru/skillbox/authentication/service/security/jwt/JwtService.java
+++ authentication/src/main/java/ru/skillbox/authentication/service/security/jwt/JwtService.java
@@ -1,5 +1,7 @@
 package ru.skillbox.authentication.service.security.jwt;
 
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -24,9 +26,11 @@ public class JwtService {
     private final Algorithm algorithm;
     private final Key key;
     private final Duration tokenExpiration;
+    private static final String ISSUER = "http://skillbox.ru";
 
     public String generateJwtToken(AppUserDetails userDetails) {
         return JWT.create()
+                .withIssuer(ISSUER)
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(new Date().getTime() + tokenExpiration.toMillis()))
                 .withSubject(userDetails.getEmail())
@@ -38,6 +42,7 @@ public class JwtService {
 
     public String generateJwtTokenFromUser(User user) {
         return JWT.create()
+                .withIssuer(ISSUER)
                 .withSubject(user.getEmail())
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(new Date().getTime() + tokenExpiration.toMillis()))
@@ -48,5 +53,14 @@ public class JwtService {
 
     public Claims getAllClaimsFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String getEmail(String token) {
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer(ISSUER)
+                .build();
+
+        DecodedJWT jwt = verifier.verify(token);
+        return jwt.getSubject();
     }
 }
